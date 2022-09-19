1bfb2dd4e67b8682df923a385309f433812de33a
Erika <erikaladner5@gmail.com>
2022-09-19 18:28:56 -0300

    mas

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import './App.css';
 import Nav from './components/Nav';
 import Footer from './components/Footer';
 import Carrousel from './components/Carrousel';
+import Premios from './components/Premios';
 
 function App() {
   return (
     <div className="App">
       <Nav />
       <Carrousel />
+      <Premios />
       <Footer />
     </div>
   );
---
+++ src/components/Premios.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import img from './../assest/charlotte.jpg'
+import './Premios.css'
+
+export default function Premios(){
+    return(
+        <div className="container" id="contenedor">
+        <h1 className="col text-center">PREMIOS</h1>
+        <hr />
+        <div className="row">
+        
+            <div className="col-md-4">
+            <div className="card">
+            <img src={img} className="card-img-top" alt="charlotte"/>
+             <div className="card-body">
+             <h5 className="card-title">PRIMER PREMIO</h5>
+            <p className="card-text">Entradas VIP para Charlotte de Witte 5/9/2022 Mandarine Park + ARS $225.000</p>
+    
+             </div>
+            </div>
+            </div>
+
+
+            <div className="col-md-4">
+            <div className="card">
+            <img src={img} className="card-img-top" alt="charlotte"/>
+             <div className="card-body">
+             <h5 className="card-title">SEGUNDO PREMIO</h5>
+            <p className="card-text">Entradas Campo Gral para Charlotte de Witte 5/9/2022 Mandarine Park + ARS $150.000</p>
+        
+             </div>
+            </div>
+            </div>
+
+
+            <div className="col-md-4">
+            <div className="card">
+            <img src={img} className="card-img-top" alt="charlotte"/>
+             <div className="card-body">
+             <h5 className="card-title">TERCER PREMIO</h5>
+            <p className="card-text">2 Tours de viernes a domingo para Expo Cannabis + ARS $100.000</p>
+        
+             </div>
+            </div>
+            </div>
+        </div>
+        </div>
+    )
+}
