6800fc46b81648e95e16fb00a1f4127d928916f3
Erika <erikaladner5@gmail.com>
2022-09-19 20:57:08 -0300

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import './Footer.css'
-import logo from './../assest/Asset-20.png'
+
 
 function Footer(){
     return(
         <div className='footer'>
-            <div className='datos'>
-                <img  src={logo} alt="logo" className='img'/>
-            </div>
+        
         </div>
     )
 }
