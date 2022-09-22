7b6996d97fa068e53659bf818694d23d69d255d2
Erika <erikaladner5@gmail.com>
2022-09-22 16:23:18 -0300

    boton

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import Carrousel from './components/Carrousel';
 import Premios from './components/Premios';
 import Info from './components/Información';
 import Formulario from './components/Formulario';
-import Sorteo from './components/Sorteo';
 import Presentación from './components/Presentación';
 
 function App() {
