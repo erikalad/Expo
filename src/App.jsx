ece18eee69753ac665c8587682a4c56baf27bd4d
Erika <erikaladner5@gmail.com>
2022-09-24 22:45:24 -0300

    ruteo

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import React from "react";
+import { Route, BrowserRouter } from "react-router-dom";
 import './App.css';
 import Nav from './components/Nav';
 import Footer from './components/Footer';
@@ -7,27 +9,36 @@ import Info from './components/Información';
 import Formulario from './components/Formulario';
 import Presentación from './components/Presentación';
 import EntradasSorteo from './components/EntradasSorteo';
-
 import CuentaRegresiva from './components/CuentaRegresiva';
+import Sorteo from './components/Sorteo'
 
 
 
 function App() {
   return (
-    <div className="App" >
+    <BrowserRouter>
+   
       <Nav />
+      <Route exact path="/">
+      <br/>
+      <Formulario />
+      </Route>
+      <Route exact path="/admin1410">
+      <br/>
+      <Sorteo />
+      </Route>
+      <Route exact path="/home">
       <CuentaRegresiva />
       <Carrousel />
       <Presentación />
       <Premios />
       <Info />
-      <Formulario/>
       <EntradasSorteo />
+      </Route>
       <br/>
       <Footer />
 
-     
-    </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,13 +1,49 @@
 import React from "react";
 import './Formulario.css'
+import Swal from "sweetalert2";
 
 
 export default function Formulario(){
+
+    
+
+
     return(
-        <div>
+        <div className="card col-10" id="form">
             <form>
-                
+            <h1>Inscripción al sorteo</h1>
+            <br />
+            <hr />
+            <h3>Datos del Comprador</h3>
+            <div className="mb-3 row">
+                <label for="staticEmail" className="col-sm-2 col-form-label">Nombre y Apellido</label>
+                <div className="col-sm-10">
+                <input type="text" readonly className="form-control" id="staticEmail" placeholder="Nombre y Apellido"/>
+                </div>
+            </div>
+            <div className="mb-3 row">
+                <label for="inputPassword" className="col-sm-2 col-form-label">Email</label>
+                <div className="col-sm-10">
+                <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@ejemplo.com"/>
+                </div>
+            </div>
+            
+            <div className="mb-3 row">
+                <label for="inputPassword" className="col-sm-2 col-form-label">Teléfono</label>
+                <div className="col-sm-10">
+                <input type="teléfono" className="form-control" id="exampleFormControlInput1" placeholder="Teléfono"/>
+                </div>
+            </div>
+            <div className="mb-3 row">
+                <label for="inputPassword" className="col-sm-2 col-form-label">DNI</label>
+                <div className="col-sm-10">
+                <input type="dni" className="form-control" id="exampleFormControlInput1" placeholder="DNI"/>
+                </div>
+            </div>
+            <button id="boton" type="button" className="btn btn-dark">Comprar entrada</button>
             </form>
         </div>
     )
 }
+
+
